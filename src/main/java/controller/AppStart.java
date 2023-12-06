20cf40f647302a88993df4b1be9ab77f7a155118
Justin Knight <77851402+Krohncat@users.noreply.github.com>
2023-12-05 20:16:30 -0500

    Central controller with main function

---
+++ src/main/java/controller/AppStart.java
@@ -0,0 +1,36 @@
+package main.java.controller;
+
+import main.java.gui.InventoryPanel;
+import main.java.gui.LoginWindow;
+import main.java.gui.ProductBrowser;
+import main.java.model.Database;
+import main.java.model.User;
+
+public class AppStart
+{
+    public static void main(String[] args) {
+        Database data = new Database();
+        LoginWindow login = new LoginWindow();
+        ProductBrowser productBrowser = new ProductBrowser();
+        InventoryPanel inventoryPanel = new InventoryPanel();
+
+
+        // Action listener for the login button
+        //TODO Establish whether a separate class should be created for button listeners with custom logic
+        login.addListener(e -> {
+            String username = login.userText.getText();
+            String password = new String(login.passwordText.getPassword());
+            User user = data.getUser(username, password);
+            if (user == null) {
+            } else {
+                if (user.isSeller()) {
+                    inventoryPanel.open();
+                } else {
+                    productBrowser.open();
+                }
+
+            }
+
+        });
+    }
+}
