729d30b55bf5192fd735cd881b7ecbb68d5ec367
Krohncat <knightj2020@fau.edu>
2023-12-06 16:37:31 -0500

    Added Populator, added updateProduct()

---
+++ src/main/java/controller/Populator.java
@@ -0,0 +1,73 @@
+package main.java.controller;
+
+import main.java.model.Database;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Populator
+{
+    public static void main(String[] args)
+    {
+        Populator pop = new Populator();
+        Database data = Database.getInstance();
+
+            File user_file = new File("sample_users.txt");
+            pop.populateUsers(data, user_file);
+            File product_file = new File("sample_products.txt");
+            pop.populateInventory(data, product_file);
+
+        {}
+    }
+
+    public void populateUsers(Database data, File file)
+    {
+        Scanner input = null;
+        try {
+            input = new Scanner(file);
+        }
+        catch(FileNotFoundException e)
+        {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        while (input.hasNextLine())
+        {
+            String username = input.nextLine();
+            String password = input.nextLine();
+            boolean isSeller = Boolean.parseBoolean(input.nextLine());
+            try {
+                data.createUser(username, password, isSeller);
+            }catch(IllegalArgumentException e){}
+        }
+        data.exportData("user");
+        input.close();
+    }
+
+    public void populateInventory(Database data, File file)
+    {
+        {
+            Scanner input = null;
+            try {
+                input = new Scanner(file);
+            }
+            catch(FileNotFoundException e)
+            {
+                e.printStackTrace();
+                System.exit(0);
+            }
+            while (input.hasNextLine())
+            {
+                String name = input.nextLine();
+                double price = Double.parseDouble(input.nextLine());
+                int quantity = Integer.parseInt(input.nextLine());
+                String seller = input.nextLine();
+                data.addProduct(name,price,quantity,seller);
+            }
+            data.exportData("inventory");
+            input.close();
+        }
+    }
+}
+
