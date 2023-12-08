293c3851536bf7ddbcced9005a7f862ee9a3f041
JoshuaF023 <JoshuaFrederick100@gmail.com>
2023-12-07 20:40:26 -0500

    Login Screen update

--- src/main/java/model/Database.java
+++ src/main/java/model/Database.java
@@ -1,5 +1,7 @@
 package main.java.model;
 
+import main.java.gui.DatabaseObserver;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
@@ -7,7 +9,9 @@ import java.util.Observable;
 
 public class Database extends Observable
 {
+
     private static Database data = null;
+    private static final DatabaseObserver observer = DatabaseObserver.getInstance();
     private ArrayList<Product> inventory;
     private ArrayList<User> users;
     private Database()
